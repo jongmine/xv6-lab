d64a601b199aa2694612dbe57a1a86f37df3aee6
jongmine <jongmine.ch@gmail.com>
2024-06-04 03:35:25 +0900

    fix: printpt 함수 수정 2

--- xv6/proc.c
+++ xv6/proc.c
@@ -640,28 +640,42 @@ pte_t *walkpgdir(pde_t *pgdir, const void *va, int alloc);
 int
 printpt(int pid)
 {
-    //need to get last level page table and print it
-    struct proc *p;
+    struct proc *p = 0;
+    pde_t *pgdir;
+    pte_t *pte;
+    uint va;
+    int found = 0;
+
+    // 프로세스 테이블에서 해당 pid의 프로세스를 찾기
     acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-        if(p->pid == pid){
-            cprintf("START PAGE TABLE (pid %d)\n", pid);
-            for (uint address = 0; address < p->sz; address += PGSIZE) {
-                pte_t *pte = walkpgdir(p->pgdir, (void *)address, 0);
-                if (*pte & PTE_P) {
-                    cprintf("%x P %s %s %x\n", (address >> 12),
-                            *pte & PTE_U ? "U" : "-",
-                            *pte & PTE_W ? "W" : "-",
-                            PTE_ADDR(*pte) >> PTXSHIFT);
-                } else {
-                    cprintf("--------\n");
-                }
-            }
-            cprintf("END PAGE TABLE\n");
+    for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
+        if (p->pid == pid) {
+            found = 1;
+            break;
         }
     }
-    // if there is no pid that matches just return 0 (failure essentially)
     release(&ptable.lock);
+
+    // 해당 프로세스가 존재하지 않거나, UNUSED 상태일 경우 -1 반환
+    if (!found || p->state == UNUSED) {
+        cprintf("Process with pid %d not found or is in UNUSED state\n", pid);
+        return -1;
+    }
+
+    pgdir = p->pgdir;
+
+    cprintf("START PAGE TABLE (pid %d)\n", pid);
+    for (va = 0; va < KERNBASE; va += PGSIZE) {
+        pte = walkpgdir(pgdir, (void *) va, 0);
+        if (pte && (*pte & PTE_P)) {
+            cprintf("%x P %s %s %x\n",
+                    (va >> 12),
+                    (*pte & PTE_U) ? 'U' : 'K',
+                    (*pte & PTE_W) ? 'W' : '-',
+                    PTE_ADDR(*pte));
+        }
+    }
+    cprintf("END PAGE TABLE\n");
     return 0;
 }
 
